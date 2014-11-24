213f0d953a68bbcbd1842a4fe32b12a13dd3a780
Hitoshi Yoshida <git@peria.jp>
2014-11-24 16:08:35 +0900

    Introduce perfect tournament system in Chudnovsky.

--- src/drm/chudnovsky.cc
+++ src/drm/chudnovsky.cc
@@ -8,6 +8,7 @@
 #include <ctime>
 
 #include "base/base.h"
+#include "base/prime.h"
 #include "base/time.h"
 
 namespace pi {
@@ -35,14 +36,36 @@ void Chudnovsky::Compute(int64 digits, mpf_t pi) {
   LOG(INFO) << "Time of computing: " << (all_end - all_start) << " sec.";
 }
 
+namespace {
+const int64 kTournamentLevel = 10;
+const int64 kTournamentWidth = 1 << kTournamentLevel;
+mpz_t g_factor[kTournamentLevel];
+}  // namespace
+
 void Chudnovsky::ComputeCore(int64 num_terms, mpf_t pi) {
   mpz_t a, b, c;
   mpz_inits(a, b, c, NULL);
 
+  int64 num_tournament = (num_terms + kTournamentWidth - 1) / kTournamentWidth;
+  LOG(INFO) << "Computing tournaments: " << num_tournament;
+  InitTournament(kTournamentLevel);
+
   double bs_start = base::GetTime();
-  BinarySplit(1, num_terms, a, b, c);
+  if (num_tournament == 1) {
+    LOG(INFO) << "Direct PT";
+    PerfectTournament(0, kTournamentWidth, kTournamentLevel, a, b, c);
+  } else {
+    BinarySplit(0, num_tournament, a, b, c);
+  }
   double bs_end = base::GetTime();
 
+  mpz_set_ui(c, 1);
+  for (int64 i = 0; i < kTournamentLevel; ++i) {
+    mpz_mul(c, c, g_factor[i]);
+    mpz_clear(g_factor[i]);
+  }
+  mpz_mul(a, a, c);
+  
   mpz_mul_ui(c, a, kConstB);
   mpz_mul_ui(b, b, 5);
   mpz_sub(b, c, b);
@@ -79,7 +102,8 @@ void Chudnovsky::ComputeCore(int64 num_terms, mpf_t pi) {
 void Chudnovsky::BinarySplit(int64 low, int64 up,
                              mpz_t a0, mpz_t b0, mpz_t c0) {
   if (low + 1 == up) {
-    SetValues(low, a0, b0, c0);
+    PerfectTournament(low * kTournamentWidth, kTournamentWidth, kTournamentLevel,
+                      a0, b0, c0);
     return;
   }
 
@@ -95,6 +119,52 @@ void Chudnovsky::BinarySplit(int64 low, int64 up,
   mpz_add(b0, b0, b1);
   mpz_mul(a0, a0, a1);
   mpz_mul(c0, c0, c1);
+
+  mpz_clears(a1, b1, c1, NULL);
+}
+
+void Chudnovsky::InitTournament(int64 level) {
+  for (int64 i = 0; i < kTournamentLevel; ++i)
+    mpz_init_set_ui(g_factor[i], 1);
+
+  base::Prime primes(1 << level);
+  for (int64 p = primes.GetNextPrime(); p > 0; p = primes.GetNextPrime()) {
+    if (p == 2 || p == 3)
+      continue;
+    int64 width = 2;
+    for (int64 i = 0; i < kTournamentLevel; ++i, width *= 2) {
+      if ((width / p) % 2 == 1)
+        mpz_mul_ui(g_factor[i], g_factor[i], p);
+    }
+  }
+}
+
+void Chudnovsky::PerfectTournament(int64 low, int64 width, int64 level,
+                                   mpz_t a0, mpz_t b0, mpz_t c0) {
+  DCHECK_EQ(1 << level, width);
+
+  if (width == 1) {
+    SetValues(low + 1, a0, b0, c0);
+    return;
+  }
+
+  mpz_t a1, b1, c1;
+  mpz_inits(a1, b1, c1, NULL);
+
+  int64 half = width / 2;
+  PerfectTournament(low,        half, level - 1, a0, b0, c0);
+  PerfectTournament(low + half, half, level - 1, a1, b1, c1);
+
+  mpz_mul(b0, b0, a1);
+  mpz_mul(b1, b1, c0);
+  mpz_add(b0, b0, b1);
+  mpz_mul(a0, a0, a1);
+  mpz_mul(c0, c0, c1);
+
+  DCHECK_GE(kTournamentLevel, level);
+  DCHECK_LT(0, level);
+  mpz_divexact(a0, a0, g_factor[level - 1]);
+  mpz_divexact(c0, c0, g_factor[level - 1]);
 
   mpz_clears(a1, b1, c1, NULL);
 }
